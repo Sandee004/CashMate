a7735ba99f947731010e4febabaad0530560d473
Sandee004 <102047643+Sandee004@users.noreply.github.com>
2023-09-26 18:50:06 +0000

    Changed the send page

---
+++ READ.md
@@ -0,0 +1,7 @@
+# CashMate
+CashMate is a finance based web app. Upon sign up you get a virtual card for carrying out transactions. You can also monitor expenses made every week/month
+
+## Things I learnt
+• Laernt how to create bar charts and QR codes using JavaScript
+
+• 
--- static/send.js
+++ static/send.js
@@ -1,24 +1,21 @@
-var pin1= document.getElementById("pin1");
-var pin2= document.getElementById("pin2");
-var pin3= document.getElementById("pin3");
-var pin4= document.getElementById("pin4");
-const btn= document.getElementById("btn");
-var a= document.getElementById("submit");
+document.querySelectorAll('.key').forEach(function(element) {
+  element.addEventListener('click', function() {
+    var numberInput = document.querySelector('.number-input');
+    var amount = numberInput.value;
+    var dataNumber = this.getAttribute('data-number');
 
-        a.originalLink= a.href
+    if (dataNumber === 'delete') {
+      amount = amount.slice(0, -1);
+    } else {
+      amount += dataNumber;
+    }
 
-        function movetoNext(current, nextFieldID) {  
-            if (current.value.length >= current.maxLength){  
-            document.getElementById(nextFieldID).focus();  
-            }  
-        }
+    numberInput.value = amount;
 
-        btn.addEventListener("click", function() {
-            if (pin4.value.length < 1){
-                alert("Pin should be 4 digits!!!")
-                a.href= 'javascript:void(0)';
-            }
-            else{
-            a.href= a.originalLink;
-            }
-        })
+    if (amount.length > 0) {
+      document.querySelector('.key.delete').classList.add('show');
+    } else {
+      document.querySelector('.key.delete').classList.remove('show');
+    }
+  });
+});
